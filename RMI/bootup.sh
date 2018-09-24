d315c613ccefaae144d66a839a9d3a89253252f4
Sheldon Benard <imac@Sheldons-MBP.lan>
2018-09-24 14:11:12 -0400

    Refactor

--- RMI/bootup.sh
+++ RMI/bootup.sh
@@ -1,9 +1,9 @@
-osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/Server; make clean; make; ./run_middleware.sh Flights,localhost,1099 Cars,localhost,1099 Rooms,localhost,1099"'
+osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/RMI/Server; make clean; make; ./run_middleware.sh Flights,localhost,1099 Cars,localhost,1099 Rooms,localhost,1099"'
 
 sleep 5
 
-osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/Server; ./run_server.sh Flights"'
-osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/Server; ./run_server.sh Cars"'
-osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/Server; ./run_server.sh Rooms"'
+osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/RMI/Server; ./run_server.sh Flights"'
+osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/RMI/Server; ./run_server.sh Cars"'
+osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/RMI/Server; ./run_server.sh Rooms"'
 
-osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/Client; make clean; make; ./run_client.sh"'
+osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/RMI/Client; make clean; make; ./run_client.sh"'

8d365f489b6f0eebc99f220c2d1a6ed617b9a03a
Sheldon <sheldon.benard@mail.mcgill.ca>
2018-09-27 21:52:41 -0400

    Adjust bootup for TCP

--- TCP/bootup.sh
+++ TCP/bootup.sh
@@ -1,9 +1,9 @@
-osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/RMI/Server; make clean; make; ./run_middleware.sh Flights,localhost,1099 Cars,localhost,1099 Rooms,localhost,1099"'
+osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/TCP/Server; make clean; make; ./run_middleware.sh localhost,6667 localhost,6668 localhost,6669"'
 
 sleep 5
 
-osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/RMI/Server; ./run_server.sh Flights"'
-osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/RMI/Server; ./run_server.sh Cars"'
-osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/RMI/Server; ./run_server.sh Rooms"'
+osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/TCP/Server; ./run_server.sh Flights,6667"'
+osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/TCP/Server; ./run_server.sh Cars,6668"'
+osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/TCP/Server; ./run_server.sh Rooms,6669"'
 
-osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/RMI/Client; make clean; make; ./run_client.sh"'
+osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/Github/DistributedSystem/TCP/Client; make clean; make; ./run_client.sh"'
